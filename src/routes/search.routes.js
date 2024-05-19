985a087164bc4c85a7f0df009ec51331551fae0b
BYAndres-GPT <baudilioandresabso@ufps.edu.co>
2024-05-18 20:20:21 -0500

    pregunta 15

--- src/routes/search.routes.js
+++ src/routes/search.routes.js
@@ -4,6 +4,7 @@ import {
   searchTwelve,
   searchThirteen,
   searchFourteen,
+  searchFifteen,
 } from "../controllers/search.controller.js";
 
 const router = express.Router();
@@ -13,5 +14,6 @@ router.get("/11", searchEleven);
 router.get("/12", searchTwelve);
 router.get("/13", searchThirteen);
 router.get("/14", searchFourteen);
+router.get("/15", searchFifteen);
 
 export default router;
