55cfb7906ae3fd42adbc5b68093137d3d3a7b035
cheo0405 <carlosalfredols@ufps.edu.co>
2024-05-19 00:49:34 -0500

    Respuesta desde el frontend de las preguntas

--- frontend/src/components/Buscador.jsx
+++ frontend/src/components/Buscador.jsx
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
 import { IoSearchCircleSharp, IoCloseOutline } from "react-icons/io5";
 import axios from "axios";
+import Encontrado from "./Encontrado";
+import NoEncontrado from "./NoEncontrado";
+import NavBar from "../components/NavBar";
+import Preguntas from "../components/Preguntas";
 
 function Buscador() {
   const [inputValue, setInputValue] = useState("");
+  const [serverResponse, setServerResponse] = useState("");
+  const [error, setError] = useState("");
 
   const handleChange = (e) => {
     setInputValue(e.target.value);
@@ -20,39 +26,66 @@ function Buscador() {
           "http://localhost:4000/defineQuestion",
           { question: inputValue }
         );
-        console.log("Mensaje del servidor:", response.data);
+        setServerResponse(response.data);
+        setError("");
       } catch (error) {
         console.error("Error al realizar la solicitud:", error);
+        setError("Error al realizar la solicitud");
+        setServerResponse("");
       }
     }
   };
+  const [backgroundColor, setBackgroundColor] = useState("bg-while");
+
+  const handleBackgroundChange = () => {
+    setBackgroundColor(
+      backgroundColor === "bg-while" ? "bg-black" : "bg-while"
+    );
+  };
 
   return (
-    <div className="bg-green-300 flex flex-col items-center justify-center p-20">
-      <h1 className="text-3xl mb-10 font-serif font-bold">
-        ¿Qué quieres consultar hoy?
-      </h1>
+    <>
+      <NavBar onBackgroundChange={handleBackgroundChange} />
 
-      <div className="flex items-center border rounded-full p-2 mt-4 bg-white shadow-lg w-9/12 relative">
-        <input
-          type="text"
-          placeholder="Escribe aquí tu pregunta..."
-          className="flex-grow outline-none pl-6 w-full"
-          value={inputValue}
-          onChange={handleChange}
-        />
-        {inputValue.length > 0 && (
-          <IoCloseOutline
-            className="text-green-300 text-3xl ml-2 cursor-pointer"
-            onClick={clearInput}
+      <div className="bg-green-300 flex flex-col items-center justify-center p-20">
+        <h1 className="text-3xl mb-10 font-serif font-bold">
+          ¿Qué quieres consultar hoy?
+        </h1>
+
+        <div className="flex items-center border rounded-full p-2 mt-4 bg-white shadow-lg w-9/12 relative">
+          <input
+            type="text"
+            placeholder="Escribe aquí tu pregunta..."
+            className="flex-grow outline-none pl-6 w-full"
+            value={inputValue}
+            onChange={handleChange}
           />
-        )}
-        <IoSearchCircleSharp
-          className="text-green-300 text-3xl ml-2 cursor-pointer"
-          onClick={handleSearch}
-        />
+          {inputValue.length > 0 && (
+            <IoCloseOutline
+              className="text-green-300 text-3xl ml-2 cursor-pointer"
+              onClick={clearInput}
+            />
+          )}
+          <IoSearchCircleSharp
+            className="text-green-300 text-3xl ml-2 cursor-pointer"
+            onClick={handleSearch}
+          />
+        </div>
       </div>
-    </div>
+
+      {error && <NoEncontrado backgroundColor={backgroundColor} />}
+
+      {serverResponse && (
+        <Encontrado
+          backgroundColor={backgroundColor}
+          mensaje={serverResponse.mensaje}
+        />
+      )}
+
+      {!error && !serverResponse && (
+        <Preguntas backgroundColor={backgroundColor} />
+      )}
+    </>
   );
 }
 
--- frontend/src/components/Encontrado.jsx
+++ frontend/src/components/Encontrado.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import encontrado from "../images/encontrado.jpg";
 
-function Encontrado({ backgroundColor }) {
+function Encontrado({ backgroundColor, mensaje}) {
   // Determina el color del texto basado en el color de fondo
   const textColor = backgroundColor === "bg-while" ? "text-black" : "text-white";
 
@@ -15,13 +15,7 @@ function Encontrado({ backgroundColor }) {
         <h1 className={`text-3xl text-center mb-10 font-serif font-bold ${textColor}`} style={{ textShadow: '0px 2px 4px #98FFD9' }}>Resultado Encontrado</h1>
 
         <h2 className={`text-2xl text-center mb-10 font-serif ${textColor}`}>
-          La plataforma ofrece una variedad de estrategias y herramientas 
-          diseñadas para mejorar habilidades cognitivas y emocionales mediante 
-          el modelo COGAF. Esto incluye ejercicios prácticos para identificar y
-          gestionar emociones, técnicas de reestructuración cognitiva, actividades
-          de autoconciencia y recursos educativos sobre el modelo COGAF. También
-          proporciona acceso a programas de entrenamiento y herramientas interactivas
-          para aplicar estos principios en situaciones reales.
+          {mensaje}
         </h2>
       </div>
     </div>
