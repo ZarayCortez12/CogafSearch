f74a76b50c3cbf5ead14557689cbc8fa3fd936b3
BYAndres-GPT <baudilioandresabso@ufps.edu.co>
2024-06-22 16:50:59 -0500

    preguntas 3, 4, 5, 6, 7

--- src/controllers/newSearhc.controller.js
+++ src/controllers/newSearhc.controller.js
@@ -2,13 +2,14 @@ import Complementary_activity_cognitive_function from "../models/complementary_a
 import Complementary_activity from "../models/complementary_activity.js";
 import Mechanic from "../models/mechanic.js";
 import Mechanic_complementary_activity from "../models/mechanic_complementary_activity.js";
+import Basic_emotion_type from "../models/basic_emotion_type.js";
+import Secondary_emotion_type from "../models/secondary_emotion_type.js";
 import { Sequelize } from "sequelize";
 import sequelize from "../database/db.js";
 
 export const defineQuestion = async (req, res) => {
   const { question } = req.body;
 
-  // hacer un switch que compare si question es 1, 2 o 9
   switch (question) {
     case 1:
       const question1 = await Complementary_activity_cognitive_function.findAll(
@@ -136,16 +137,220 @@ export const optionQuestion = async (req, res) => {
             message: "No se encontraron registros con el nombre dado.",
           });
         }
-        const descriptions = results.map((result) => result.description).join(', ');
+        const descriptions = results
+          .map((result) => result.description)
+          .join(", ");
         return res.status(200).json({
           message: `Las funciones cognitivas en la ${option} son: ${descriptions}`,
         });
 
-      case 4:        
+      case 4:
+        const [cognitiveTestResults] = await sequelize.query(
+          `SELECT ccft.description
+             FROM cognitive_test ct
+             JOIN psychological_task_cognitive_test ptct ON ct.id = ptct.cognitive_test_id
+             JOIN psychological_task pt ON ptct.psychological_task_id = pt.id
+             JOIN psychological_task_cognitive_function ptcf ON pt.id = ptcf.psychological_task_id
+             JOIN cognitive_function cf ON ptcf.cognitive_function_id = cf.id
+             JOIN complex_cognitive_function ccf ON cf.id = ccf.id
+             JOIN complex_cognitive_function_type ccft ON ccf.complex_cognitive_function_type_id = ccft.id
+             WHERE ct.name = '${option}'`
+        );
+        if (!cognitiveTestResults || cognitiveTestResults.length === 0) {
+          return res.status(404).json({
+            message: "No se encontraron registros con el nombre del test dado.",
+          });
+        }
+        const cognitiveTestDescriptions = cognitiveTestResults
+          .map((result) => result.description)
+          .join(", ");
         return res.status(200).json({
-          message: "fafha.",
+          message: `Las funciones cognitivas complejas en el test ${option} son: ${cognitiveTestDescriptions}`,
         });
-      
+
+      case 5:
+        const [psychologicalTaskResults] = await sequelize.query(
+          `SELECT pt.name
+           FROM cognitive_test ct
+           JOIN psychological_task_cognitive_test ptct ON ct.id = ptct.cognitive_test_id
+           JOIN psychological_task pt ON ptct.psychological_task_id = pt.id
+           WHERE ct.name = '${option}'`
+        );
+        if (
+          !psychologicalTaskResults ||
+          psychologicalTaskResults.length === 0
+        ) {
+          return res.status(404).json({
+            message: "No se encontraron registros con el nombre del test dado.",
+          });
+        }
+        const psychologicalTaskNames = psychologicalTaskResults
+          .map((result) => result.name)
+          .join(", ");
+        return res.status(200).json({
+          message: `Las tareas psicológicas en el test ${option} son: ${psychologicalTaskNames}`,
+        });
+      case 6:
+        // Verificar si 'option' está en basic_emotion_type
+        let basicEmotionTypeResults;
+        try {
+          basicEmotionTypeResults = await Basic_emotion_type.findAll({
+            where: {
+              description: option,
+            },
+            attributes: ["id"],
+          });
+        } catch (error) {
+          console.error("Error al buscar en basic_emotion_type:", error);
+          return res.status(500).json({
+            message: "Error al buscar en basic_emotion_type.",
+          });
+        }
+
+        // Verificar si 'option' está en secondary_emotion_type
+        let secondaryEmotionTypeResults;
+        try {
+          secondaryEmotionTypeResults = await Secondary_emotion_type.findAll({
+            where: {
+              description: option,
+            },
+            attributes: ["id"],
+          });
+        } catch (error) {
+          console.error("Error al buscar en secondary_emotion_type:", error);
+          return res.status(500).json({
+            message: "Error al buscar en secondary_emotion_type.",
+          });
+        }
+
+        if (
+          basicEmotionTypeResults.length === 0 &&
+          secondaryEmotionTypeResults.length === 0
+        ) {
+          return res.status(404).json({
+            message:
+              "La opción no existe en basic_emotion_type ni en secondary_emotion_type.",
+          });
+        }
+
+        // Si 'option' está en basic_emotion_type
+        if (basicEmotionTypeResults.length > 0) {
+          const basicEmotionTypeId = basicEmotionTypeResults[0].id;
+
+          // Consulta para obtener la descripción de complex_cognitive_function_type
+          try {
+            const [results] = await sequelize.query(`
+        SELECT ccf_type.description
+        FROM basic_emotion_type bet
+        JOIN basic_emotion be ON bet.id = be.basic_emotion_type_id
+        JOIN emotion e ON be.id = e.id
+        JOIN emotion_cognitive_function ecf ON e.id = ecf.emotion_id
+        JOIN cognitive_function cf ON ecf.cognitive_function_id = cf.id
+        JOIN complex_cognitive_function ccf ON cf.id = ccf.id
+        JOIN complex_cognitive_function_type ccf_type ON ccf.complex_cognitive_function_type_id = ccf_type.id
+        WHERE bet.id = ${basicEmotionTypeId}
+      `);
+
+            const descriptions = results
+              .map((result) => result.description)
+              .join(", ");
+            return res.status(200).json({
+              message: `Las funciones cognitivas complejas para el tipo de emoción básica ${option} son: ${descriptions}`,
+            });
+          } catch (error) {
+            console.error(
+              "Error al ejecutar la consulta para basic_emotion_type:",
+              error
+            );
+            return res.status(500).json({
+              message:
+                "Error al obtener las funciones cognitivas complejas para el tipo de emoción básica.",
+            });
+          }
+        }
+
+        // Si 'option' está en secondary_emotion_type
+        if (secondaryEmotionTypeResults.length > 0) {
+          const secondaryEmotionTypeId = secondaryEmotionTypeResults[0].id;
+
+          // Consulta para obtener la descripción de complex_cognitive_function_type
+          try {
+            const [results] = await sequelize.query(`
+        SELECT ccf_type.description
+        FROM secondary_emotion_type scet
+        JOIN secondary_emotion se ON scet.id = se.secondary_emotion_type_id
+        JOIN emotion e ON se.id = e.id
+        JOIN emotion_cognitive_function ecf ON e.id = ecf.emotion_id
+        JOIN cognitive_function cf ON ecf.cognitive_function_id = cf.id
+        JOIN complex_cognitive_function ccf ON cf.id = ccf.id
+        JOIN complex_cognitive_function_type ccf_type ON ccf.complex_cognitive_function_type_id = ccf_type.id
+        WHERE scet.id = ${secondaryEmotionTypeId}
+      `);
+
+            const descriptions = results
+              .map((result) => result.description)
+              .join(", ");
+            return res.status(200).json({
+              message: `Las funciones cognitivas complejas para el tipo de emoción secundaria ${option} son: ${descriptions}`,
+            });
+          } catch (error) {
+            console.error(
+              "Error al ejecutar la consulta para secondary_emotion_type:",
+              error
+            );
+            return res.status(500).json({
+              message:
+                "Error al obtener las funciones cognitivas complejas para el tipo de emoción secundaria.",
+            });
+          }
+        }
+
+      case 7:
+        try {
+          const query = `
+              SELECT c.name AS characteristic_description, p.value AS parameter_value
+              FROM emotion e
+              JOIN emotion_characteristic ec ON e.id = ec.emotion_id
+              JOIN characteristic c ON ec.characteristic_id = c.id
+              JOIN parameter p ON c.id = p.characteristic_id
+              LEFT JOIN basic_emotion be ON e.id = be.id
+              LEFT JOIN basic_emotion_type bet ON be.basic_emotion_type_id = bet.id
+              LEFT JOIN secondary_emotion se ON e.id = se.id
+              LEFT JOIN secondary_emotion_type scet ON se.secondary_emotion_type_id = scet.id
+              WHERE bet.description = '${option}' OR scet.description = '${option}';
+            `;
+
+          const results = await sequelize.query(query, {
+            type: sequelize.QueryTypes.SELECT,
+          });
+
+          if (!results || results.length === 0) {
+            return res.status(404).json({
+              message: `No se encontraron características y parámetros para la emoción con descripción '${option}'.`,
+            });
+          }
+
+          // Construir la respuesta deseada
+          let characteristicsMessage = `Características y parámetros para la emoción con descripción '${option}': `;
+
+          results.forEach((result, index) => {
+            if (index > 0) {
+              characteristicsMessage += ", ";
+            }
+            characteristicsMessage += `${result.parameter_value} ${result.characteristic_description}`;
+          });
+
+          return res.status(200).json({
+            message: characteristicsMessage,
+          });
+        } catch (error) {
+          console.error("Error al ejecutar la consulta:", error);
+          return res.status(500).json({
+            message:
+              "Error al obtener características y parámetros para la emoción.",
+          });
+        }
+
       default:
         return res.status(400).json({
           message: "Pregunta no válida.",
