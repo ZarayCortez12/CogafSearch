2fab353d45db2b5407bea8a175f581ce9f923d2b
BYAndres-GPT <baudilioandresabso@ufps.edu.co>
2024-06-23 10:02:46 -0500

    commit

--- frontend/src/components/Buscador.jsx
+++ frontend/src/components/Buscador.jsx
@@ -11,7 +11,7 @@ function Buscador() {
   const handleSearchQuestions = async () => {
     try {
       const response = await axios.get("http://localhost:4000/search");
-      setQuestions(response.data.question);
+      console.log(response.data);
     } catch (error) {
       console.error("Error al realizar la solicitud:", error);
     }
